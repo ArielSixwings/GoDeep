92a081dd1cea6cd15ded6d9ffa265436c7892d98
arielsixwings <arielserafim@gmail.com>
2021-02-20 04:42:56 -0300

    Energy feature extration beta function,(not working yet)

--- code/imageprocessing/FeatureExtration.go
+++ code/imageprocessing/FeatureExtration.go
@@ -2,13 +2,15 @@ package imageprocessing
 
 import (
 	"gocv.io/x/gocv"
-	// "fmt"
+	"fmt"
+	"math"
+	
 
 )
 
 /**
  * [Take the GLCM and the means of a group of images]
- * @param {[type]} Images []gocv.Mat [description]
+ * @param {[type]} Images []gocv.mat [description]
  * @param {[type]} GLCMs  []gocv.Mat [description]
  * @param {[type]} means  []gocv.Mat [description]
  * @param {[type]} show   bool       [description]
@@ -19,14 +21,38 @@ func GroupGLCM(Images []gocv.Mat, GLCMs []gocv.Mat, means []gocv.Mat, show bool)
 
 	mean := gocv.NewMat()
 	
-	for i := 0; i < 50; i++ {
+	for i := 0; i < len(Images); i++ {
+		fmt.Println("Calculating GLCM   ", i, "of ", len(Images))
 		gocv.CalcCovarMatrix(Images[i], &GLCM, &mean, gocv.CovarCols, Images[2].Type())
 
 		GLCMs[i] = GLCM
 		means[i] = mean
-
 		if show {
 			ShowImage("And this is yout image", GLCMs[i], 100)
 		}
 	}
+}
+
+/**
+ * [getEnergy description]
+ * @param  {[type]} GLCM gocv.Mat      [A GLCM gocv.Mat]
+ * @return {[type]} float64 		   [Energy of the image that produced the GLCM]
+ */
+func Energy(GLCM gocv.Mat) float64{
+	var Energy float64 = 0
+	for r := 0; r < GLCM.Rows()	; r++ {
+		for c := 0; c < GLCM.Cols(); c++ {
+			Energy += float64(math.Pow(float64(GLCM.GetUCharAt(r,c)),2))
+		}
+	}
+	return Energy
+}
+
+func GroupEnergy(GLCMs []gocv.Mat, Energys []float64){
+
+	for i := 0; i < len(GLCMs); i++ {
+		fmt.Println("Calculating Energy:  ",i, "of ",len(GLCMs))
+		Energys[i] = Energy(GLCMs[i])	
+	}
+
 }
--- code/test.go
+++ code/test.go
@@ -3,8 +3,8 @@ package main
 import (
 	"./imageprocessing"
 	"gocv.io/x/gocv"
-	// "fmt"
-
+	"fmt"
+	//"math"
 )
 /** CovarFlags
 	// CovarScrambled indicates to scramble the results.
@@ -26,25 +26,53 @@ import (
 	CovarCols CovarFlags = 16
 **/
 
-/**
- *
- * @brief      { function_description }
- * @return     { description_of_the_return_value }
- */
+// func getEnergy(GLCM gocv.Mat) {
+// 	var Energy float64 = 0
+// 	for r := 0; r < GLCM.Rows()	; r++ {
+// 		for c := 0; c < GLCM.Cols(); c++ {
+// 			Energy += float64(math.Pow(float64(GLCM.GetUCharAt(r,c)),2))
+// 		}
+// 	}
+// 	fmt.Println(Energy)
+// 	//return Energy
+// }
+
 func main() {
 	
 	var size int
 	
+	//var normtype gocv.NormType = gocv.NormMinMax
+
 	size  = imageprocessing.FolderLength("./imageprocessing/Images/danger")
 
 	Images := make([]gocv.Mat,size)
 
 	GLCMs := make([]gocv.Mat,size)
 
+	//normalizedGLCMs := make([]gocv.Mat,size)
+
 	means := make([]gocv.Mat,size)
 
 	imageprocessing.ReadFolder(Images,"./imageprocessing/Images/danger",true,false,false)
 	
 	//GroupGLCM(Images []gocv.Mat, GLCMs []gocv.Mat, means []gocv.Mat, show bool)
-	imageprocessing.GroupGLCM(Images, GLCMs, means, true)
+	imageprocessing.GroupGLCM(Images, GLCMs, means, false)
+	// var Energy float64 =  imageprocessing.Energy(GLCMs[1]) 
+	// fmt.Println("Energy:    ", Energy)
+	
+	Energys := make([]float64,size)
+	imageprocessing.GroupEnergy(GLCMs,Energys)
+	for i := 0; i < size; i++ {
+		fmt.Println("Energy:   ", Energys[i])
+	}
+
+	//func Normalize(src Mat, dst *Mat, alpha float64, beta float64, typ NormType)
+	//min value of dst is alpha and max value of dst is beta
+	// for i := 0; i < size; i++ {
+	// 	gocv.Normalize(GLCMs[i], &normalizedGLCMs[i], 0.0, 255.0, normtype )
+	// 	//imageprocessing.ShowImage("normalizedGLCMs", normalizedGLCMs[i], 100)
+	// }
+	
+	 
+	
 }
