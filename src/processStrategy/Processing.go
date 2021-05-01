51eeba5219ac2775919fcab14ecf08444bb51521
arielsixwings <arielserafim@gmail.com>
2021-05-01 13:32:46 -0300

    renamed some modules and folders

--- src/processStrategy/Processing.go
+++ src/processStrategy/Processing.go
@@ -1,1 +1,11 @@
 package process
+
+func (dp *DataProcessing) SetProcessStrategy(ps processStrategy) {
+	dp.Strategy = ps
+}
+
+func (dp *DataProcessing) GroupProcess(){
+	for i := 0; i < (*dp).Readinfo.SizeData; i++ {
+		(*dp).Strategy.Process()
+	}
+}
--- src/processStrategy/StructProcessing.go
+++ src/processStrategy/StructProcessing.go
@@ -4,13 +4,15 @@ import(
 	"../basicdata"
 )
 
-type learnStrategy interface {
-	Process(dp *DataProcessing)
+type processStrategy interface {
+	Process() error
+	Allocate() error
+	SaveProcessedData() error 
+	PresentProcessing(Menssage string, index int, time int) error
 }
 
-type DataProcessing struct {
+type DataProcessing struct{
+	Strategy processStrategy
 
+	Readinfo 	cartesian.ReadInformation
 }
-
-
-type ByDist []cartesian.Featurepoint
