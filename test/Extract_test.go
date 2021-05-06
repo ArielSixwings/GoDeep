1497d86518d590508193414f1de81083b3b78e74
jackesfonseca <jackesdafonseca@gmail.com>
2021-05-06 13:48:16 -0300

    working on test

--- test/Extract_test.go
+++ test/Extract_test.go
@@ -13,13 +13,24 @@ import (
 	//"../src/learnstrategy/nonparametric"
 )
 
+var datasetextractor extract.ImageExtractor
+
 func TestSetOrigins(t *testing.T) {
-	var datasetextractor extract.ImageExtractor
+	
 	origins := []string{"../data/ImagesData/danger", 
 		"../data/ImagesData/asphalt", 
 		"../data/ImagesData/grass"}
 
 	_, err := datasetextractor.SetOrigins(origins,&datasetextractor)
+
+	if err != nil {
+		t.Error("Unexpected value")
+	}
+}
+
+func TestRead(t *testing.T) {
+
+	err := datasetextractor.Read(false,false,true)
 
 	if err != nil {
 		t.Error("Unexpected value")
