1e6992e63eb0bfc2df4d94f99cfd45286ac186c5
arielsixwings <arielserafim@gmail.com>
2021-05-12 21:34:18 -0300

    working on colors to visualization

--- src/basicdata/colorgalery/Colors.go
+++ src/basicdata/colorgalery/Colors.go
@@ -1,1 +1,54 @@
 package rgbacolor
+
+import(
+	"image/color"
+)
+
+func red(thecolor *color.RGBA){
+	(*thecolor).R = 255
+	(*thecolor).G = 0
+	(*thecolor).B = 0
+	(*thecolor).A = 1
+}
+
+func blue(thecolor *color.RGBA){
+	(*thecolor).R = 0
+	(*thecolor).G = 0
+	(*thecolor).B = 255
+	(*thecolor).A = 1
+}
+
+func purple(thecolor *color.RGBA){
+	(*thecolor).R = 107
+	(*thecolor).G = 66
+	(*thecolor).B = 143
+	(*thecolor).A = 1
+}
+
+func copper(thecolor *color.RGBA){
+	(*thecolor).R = 184
+	(*thecolor).G = 115
+	(*thecolor).B = 51
+	(*thecolor).A = 1
+}
+
+func silver(thecolor *color.RGBA){
+	(*thecolor).R = 170
+	(*thecolor).G = 169
+	(*thecolor).B = 173
+	(*thecolor).A = 1
+}
+
+func gold(thecolor *color.RGBA){
+	(*thecolor).R = 255
+	(*thecolor).G = 215
+	(*thecolor).B = 0
+	(*thecolor).A = 1
+}
+
+func pink(thecolor *color.RGBA){
+	(*thecolor).R = 255
+	(*thecolor).G = 20
+	(*thecolor).B = 147
+	(*thecolor).A = 1
+}
--- src/painter.go
+++ src/painter.go
@@ -6,32 +6,51 @@ import (
 	"image/color"
 )
 
-func main(){
-	var Image = gocv.IMRead("background.jpg", gocv.IMReadUnchanged)
-	var center image.Point
-	var thecolor color.RGBA
-	var thesize image.Rectangle
+func PresentProcessedData(){
+	var(
+		background = gocv.IMRead("background.jpg", gocv.IMReadUnchanged)
+		
+		theRectangle image.Rectangle
 
-	thesize.Min.X = 32 
-	thesize.Min.Y = 480
-	thesize.Max.X = 64
-	thesize.Max.Y = 224
+		centergender image.Point
+		centerclass image.Point
+
+		rectanglecolor color.RGBA
+		colormale color.RGBA
+		colorclass color.RGBA
+	)
+
+	theRectangle.Min.X = 64 
+	theRectangle.Min.Y = 936
+	theRectangle.Max.X = 128
+	theRectangle.Max.Y = 132 - Age
+
+	centergender.X = 700
+	centergender.Y = 750
+
+	centerclass.X = 700
+	centerclass.Y = 250
+
+	gocv.Circle(&background, centergender, 120, colormale, 240)
+	gocv.Circle(&background, centerclass, 90, colorclass, 190)
+	gocv.Rectangle(&background, theRectangle, rectanglecolor, 64)
+}
+
+
+func main(){
+	var centergender image.Point
+	var centerclass image.Point
+
+	var 
+
 	
-	thecolor.R = 255
-	thecolor.G = 0
-	thecolor.B = 0
-	thecolor.A = 1
-	
-	center.X = 428
-	center.Y = 128
+
 	
 	window := gocv.NewWindow("the image") //basic window
 	
 	window.IMShow(Image)               //show the image
 	window.WaitKey(0)
 	
-	gocv.Circle(&Image, center, 40, thecolor, 80)
-	gocv.Rectangle(&Image, thesize, thecolor, 32)
 	
 	window.IMShow(Image)               //show the image
 	window.WaitKey(0)
